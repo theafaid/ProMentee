ff2db0c96f5ceb9fc8b7e73d6f22e4896469b16f
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-05-21 00:25:12 +0200

    install tabler template

--- public/js/app.js
+++ public/js/app.js
@@ -32143,7 +32143,7 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: '#promentee'
 });
 
 /***/ }),
--- resources/js/app.js
+++ resources/js/app.js
@@ -28,5 +28,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: '#promentee',
 });
