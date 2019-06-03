b2554e6ed5d8ade660705b08b68fae98e6c07ed0
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-03 15:20:39 +0200

    Setup profiles table

---
+++ database/migrations/2019_06_03_121317_create_profiles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->enum('gender', ['male', 'female']);
+
+            $table->string('age');
+            $table->string('pic')->nullable();
+            $table->string('motto')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('job')->nullable();
+            $table->string('description')->nullable();
+            $table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
