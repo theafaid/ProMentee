432a517c4b5c28b85177c7ce2d9a7a7f21fd075f
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-03 14:11:08 +0200

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,26 @@
 <?php
 
-
-
-Route::any('logout', 'Auth\LoginController@logout')->name('logout');
-
+/**
+ ****  Localized routes ****
+*/
 Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => [ 'localeSessionRedirect', 'localizationRedirect']
 ], function(){
-
-
     // Js Localization
     Route::get('/js/lang.js', 'JavascriptLangsController@get')->name('assets.lang');
-
+    // welcome page
     Route::get('/', function () {
         return view('welcome');
     });
     Auth::routes();
     Route::get('/home', 'HomeController@index')->name('home');
 });
+
+
+/**
+ * Unlocalized routes
+ */
+
+Route::any('logout', 'Auth\LoginController@logout')->name('logout');
+
