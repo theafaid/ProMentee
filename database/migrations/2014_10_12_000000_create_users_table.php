3de9161e140387e117c5cc050076f55bff9c4711
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-04 07:36:39 +0200

    fix profiles table err

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('provider')->nullable();
-            $table->string('provider_id');
+            $table->string('provider_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
