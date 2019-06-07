f56545bd6152474359a3b42dd9e936653c519663
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-07 08:03:26 +0200

    Edit profiles user_id col

--- database/migrations/2019_06_03_121317_create_profiles_table.php
+++ database/migrations/2019_06_03_121317_create_profiles_table.php
@@ -14,7 +14,7 @@ class CreateProfilesTable extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->bigIncrements('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')
                 ->onDelete('cascade');
 
