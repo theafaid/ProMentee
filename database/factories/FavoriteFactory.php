b022c117ab04895cdce3ff0b09ec8734ab5c5956
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-14 21:36:21 +0200

    Initial Favorite model

---
+++ database/factories/FavoriteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Favorite;
+use Faker\Generator as Faker;
+
+$factory->define(Favorite::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
