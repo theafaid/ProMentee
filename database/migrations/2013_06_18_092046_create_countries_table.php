fa7913a749f376dc0ec29e18d9d2d19bea95130e
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-18 11:24:11 +0200

    Setup countries, cities

---
+++ database/migrations/2013_06_18_092046_create_countries_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('code', 3)->unique();
+            $table->string('name', 52);
+            $table->enum('continent', ['Asia', 'Europe', 'North America', 'Africa', 'Oceania', 'Antarctica', 'South America']);
+            $table->string('region', 26);
+            $table->decimal('surface_area', 10, 2);
+            $table->integer('indep_year')->length(4);
+            $table->integer('population')->length(11)->unsigned();
+            $table->decimal('life_expectancy', 3, 1);
+            $table->decimal('gnp', 10, 2);
+            $table->decimal('gnp_old', 10, 2);
+            $table->string('local_name', 45);
+            $table->string('government_form', 45);
+            $table->string('head_of_state', 60);
+            $table->integer('capital')->length(11)->unsigned();
+            $table->string('code2', 2);
+            $table->foreign('capital', 11)->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
---
+++ database/migrations/2013_06_18_092056_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name', 35);
+            $table->string('country_code', 3);
+            $table->string('district', 20);
+            $table->integer('population', 11)->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
